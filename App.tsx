82859b64adc5fade5f04d3086f3d2aad73125c30
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-08 21:19:06 +0530

    Added Bottom Navigation

--- App.tsx
+++ App.tsx
@@ -16,22 +16,44 @@ import {
   FlatList,
   SectionList
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App = () => {
-  const datas=[ {value:50}, {value:80}, {value:90}, {value:70} ]
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+
+
+function SettingsScreen() {
+  return <Text>Ok Mamu</Text>
+}
+
+function BottomMenu() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+const Home = () => {
+  const datas=[ {value:50, label: "first"}, {value:80,label: "second"}, {value:90,label: "third"}, {value:70, label: "fourth"} ]
   const data = ['Mirjapur', "Lucknow", "Amethi",'Mirjapur', "Lucknow", "Amethi",'Mirjapur', "Lucknow", "Amethi",'Mirjapur', "Lucknow", "Amethi",'Mirjapur', "Lucknow", "Amethi"]
   const pending = [{pending: 302, site: "Mirjapur"}, {pending: 1, site: "Lucknow"}, {pending: 10034, site: "Amethi"},{pending: 302, site: "Mirjapur"}, {pending: 1, site: "Lucknow"}, {pending: 10034, site: "Amethi"}]
   return (
+
     <SafeAreaView style={styles.container}>
       <Text style={styles.header}>Welcome to S.S Furniture Steel Mart</Text>
     <View style={styles.recent}><Text style={styles.header}> Recent Work Sites</Text><FlatList data={data} renderItem={({item})=> <Text onPress={()=> {console.log(`Clicked ${item}`)}} style={styles.recentItems}>{item}</Text>} horizontal={true} showsHorizontalScrollIndicator={false}/></View>
-    <View style={{flex:1, justifyContent: "center", alignContent: "center", margin: "auto",backgroundColor: '#1A3461'}}><LineChart data={datas}  isAnimated={true}            initialSpacing={0}
+    <View style={{backgroundColor: '#1A3461', maxHeight: "auto"}}><LineChart data={datas}  isAnimated={true}            initialSpacing={0}
              
               spacing={30}
-              hideDataPoints
+              width={100}
+
+            
               thickness={5}
               hideRules
-              hideYAxisText
+            
               yAxisColor="#0BA5A4"
               showVerticalLines
               verticalLinesColor="rgba(14,164,164,0.5)"
@@ -41,11 +63,21 @@ const App = () => {
 
     <View><Text style={styles.header}>Pending Labour Cost</Text><FlatList data={pending} renderItem={({item})=> {
       return(<View style={styles.tableN}><Text style={styles.table}>{item.site}: </Text><Text style={styles.table}>{item.pending}</Text></View>)}}/></View>
-      
+
     </SafeAreaView>
     
   );
 };
+
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <BottomMenu/>
+    </NavigationContainer>
+  )
+}
 
 const styles = StyleSheet.create({
 
